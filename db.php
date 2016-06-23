ea5ad875da736a1a658e6285592fcd76e4dc62e9
Tomasz Maj <tmaj.pl@gmail.com>
2016-06-23 17:36:07 +0200

    local startup

---
+++ db.php
@@ -0,0 +1,18 @@
+<?php
+
+$dsn = 'mysql:dbname=czat;host=localhost';
+$user = 'root';
+$password = 'root';
+
+try {
+	$dbh = new PDO($dsn, $user, $password, array(
+		PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING,
+		PDO::ATTR_PERSISTENT => false // true for keeping connection alive
+	));
+} catch(PDOException $e) {
+    print "Error!: " . $e->getMessage() . "<br/>";
+    die();
+}
+
+?>
+
